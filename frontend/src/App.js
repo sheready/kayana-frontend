fff22f320f9b2a2db02d7730df8675ab6a7fed44
sheready <csstlw@gmail.com>
2023-05-31 23:05:16 +0300

    remove unnecessary file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
