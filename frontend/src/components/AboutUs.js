8b834d8ef026d09db2f789e5750df3abb753f266
sheready <csstlw@gmail.com>
2023-06-13 13:35:53 +0300

    create about us component

---
+++ frontend/src/components/AboutUs.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <>
+        <div class='flex flex-row flex-wrap w-full'>
+            <div>
+                <h1 class='mt-5 font-[Raleway] text-[25px] font-medium text-center'>ABOUT US</h1>
+            </div>
+            <div>
+                <p class='pt-6 font-[Raleway] text-[25px] text-center px-2'>Kayana is a community of female entrepreneurs of abundance that come together to move their businesses from ideation to start up. 
+                    Typically the businesses will be run from their homes or online, and the founders are looking at the next step of growth, but may not have the know-how or courage to do so.
+                </p>
+            </div>
+        </div>
+        <div class="grid grid-cols-4 gap-4 mt-2">
+            <div>
+                <img class="h-auto max-w-lg rounded-lg" src={} alt="image description"></img>
+            </div> 
+            <div>09</div>
+            <div>09</div>
+            <div>09</div>
+            <div>09</div>
+            <div>09</div>
+        </div>
+    </>
+  )
+}
+
+export default AboutUs
+
