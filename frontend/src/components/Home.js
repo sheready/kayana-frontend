391c00def4a0a9514705e76f4640cf40f159fc8b
sheready <csstlw@gmail.com>
2023-06-13 13:35:04 +0300

    add AboutUs componenet to home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import banner from '../media/banner.jpg'
+import AboutUs from './AboutUs'
 
 const Home = () => {
   return (
-    <div>
-
-      <img class=" max-w-full" src={banner} alt="image description"/>
-
-    </div>
+    <>
+      <div class="flex items-center justify-center">
+        <div class="w-full">
+          <img src={banner} alt="banner-img" class="h-30 w-full object-cover"/>
+        </div>
+      </div>
+      <AboutUs/>
+    </>
   )
 }
 
