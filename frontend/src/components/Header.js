e710be52274b455ea9fa9bfa7e2859ed45444bbf
sheready <csstlw@gmail.com>
2023-06-27 02:23:34 +0300

    edit header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,21 +4,21 @@ import logo from '../media/logo.png'
 const Header = () => {
   return (
     <>
-      <nav class="relative container mx-auto p-6">
+      <nav class="relative container mx-auto px-6">
         <div class="flex items-center justify-between">
           {/* logo */}
-          <div class="pt-2">
+          <div class="pt-9 h-[100px] w-[100px]">
             <img src={logo} alt="kayana-logo"></img>
           </div>
           {/* menu items display after medium screen else hide the items */}
-          <div class="hidden md:flex space-x-5">
+          <div class="hidden md:flex space-x-7">
             <a href="#" class="hover:text-green">Home</a>
             <a href="#" class="hover:text-green">Kayana Awards</a>
             <a href="#" class="hover:text-green">Kayana News</a>
             <a href="#" class="hover:text-green">Contact us</a>
           </div>
           {/* donate button hidden in smaller screen and shown in large screens as block */}
-          <a href='#' class="hidden md:block p-3 px-6 pt-2 mr-6 text-white bg-green rounded-full baseline hover:bg-gray">
+          <a href='#' class="hidden md:block p-3 px-6 pt-2 mr-10 text-white bg-green rounded-full baseline hover:bg-gray">
             Donate
           </a>
         </div>
