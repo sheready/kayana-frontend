1a0e4cd5c1c1dfd54b04dad5d24f1ef304bae3c7
sheready <csstlw@gmail.com>
2023-06-27 02:22:54 +0300

    add Aboutus and Newsletter component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import banner from '../media/banner.jpg'
 import AboutUs from './AboutUs'
+import Newsletter from './Newsletter'
 
 const Home = () => {
   return (
@@ -9,7 +10,7 @@ const Home = () => {
       <section id='Hero'>
         {/* flex container */}
         <div class="relative">
-          <img class="h-auto md:h-[82vh] md:w-[100vw]" src={banner}  alt='banner-image'></img>
+          <img class="h-auto md:h-[85vh] md:w-[100vw]" src={banner}  alt='banner-image'></img>
 
           <div class="flex flex-col space-y-8 md:absolute md:mb-32 left-1/2 md:space-y-12 md:top-1/2 md:left-1/4 md:-translate-x-1/2 md:-translate-y-1/2">
             <h1 class="max-w-md px-[2%] mx-[8%] text-2xl font-bold text-center md:text-white md:text-4xl md:text-left mt-2">
@@ -27,6 +28,11 @@ const Home = () => {
         </div>
 
       </section>
+      <AboutUs/>
+      <Newsletter/>
+
+      {/* newletter signup */}
+
     </>
   )
 }
