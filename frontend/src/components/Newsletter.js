f2cad827741a09fac43f39cd7abc4e5bc8ee632d
sheready <csstlw@gmail.com>
2023-06-27 02:21:51 +0300

    add newsletter form

---
+++ frontend/src/components/Newsletter.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import magazine from '../media/magazine.jpg'
+import logo from '../media/logo.png'
+
+const Newsletter = () => {
+  return (
+    <div class="container mx-auto flex flex-col md:flex-row">
+        <div class="flex flex-col space-y-10 mx-16 mt-5 md:w-1/3 md:justify-center">
+            <img src={magazine} alt="newsletter-img" class="w-[70vw]"></img>
+
+        </div>
+        <div class="flex flex-col mt-4 mx-auto space-y-6 md:w-2/3 md:justify-center">
+            <img src={logo} alt="kayana-logo" class="w-12 md:w-20 mx-auto" />
+            <h5 class='text-1xl font-primary text-gray font-bold'>SIGN UP FOR OUR NEWSPAPER</h5>
+            <form>
+                <div class="grid md:grid-cols-2 md:gap-6">
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_first_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                    </div>
+                    <div class="relative z-0 w-full mb-6 group">
+                        <input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                        <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
+                    </div>
+                </div>
+                <div class="relative z-0 w-full mb-6 group">
+                    <input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                    <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
+                </div>
+                <button type="submit" class="text-white bg-green hover:bg-gray rounded-full text-sm w-1/2 sm:w-auto px-5 py-2.5 text-center align-middle">Submit</button>
+            </form>
+
+            
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Newsletter
