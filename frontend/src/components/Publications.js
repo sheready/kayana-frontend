fc18c23ce0f8e9ff0b5039ee9edb461671851102
sheready <csstlw@gmail.com>
2023-06-30 23:56:38 +0300

    add a hook for default layout plugin

--- frontend/src/components/Publications.js
+++ frontend/src/components/Publications.js
@@ -2,25 +2,39 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/thumbnail/lib/styles/index.css';
 import React from 'react'
 import { Worker } from '@react-pdf-viewer/core';
-import { Viewer } from '@react-pdf-viewer/core';
+import { PageLayout,ViewMode,Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import w2w from '../media/publications/Kayana-News-Special-Issue-W2W-2023-final-1.pdf'
-import { thumbnailPlugin, ThumbnailDirection } from '@react-pdf-viewer/thumbnail';
+import '../style/toolbar.css'
 
 const Publications = () => {
-    const thumbnailPluginInstance = thumbnailPlugin({
-        thumbnailWidth: 80,
-    });
-    const { Thumbnails } = thumbnailPluginInstance;
+  const defaultLayoutPluginInstance = defaultLayoutPlugin => {
+    <div >
+      {defaultLayoutPlugin()}
+    </div>
+  };
+
+  const renderToolbar = toolbarSlot => {
+    return (
+      <div className="pdf-viewer-toolbar">
+        {/* Custom toolbar content */}
+        {toolbarSlot()}
+      </div>
+    );
+  };
+   
   return (
-    <div style={{ border: '1px solid rgba(0, 0, 0, 0.3)', height: '750px',}}>
-
+    <div >
+      <div> 
         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
-            <Thumbnails />
-            <Viewer fileUrl={w2w} plugins={[thumbnailPluginInstance]} />
-            
+          <Viewer fileUrl={w2w} plugins={[defaultLayoutPluginInstance]} toolbarSlot={renderToolbar}  />
         </Worker>
-
-
+      </div>
+      {/* <div style={{ width: '50%' }}>
+        <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+          <Viewer fileUrl={w2w} plugins={[defaultLayoutPluginInstance]} />
+        </Worker>
+      </div> */}
     </div>
   )
 }
