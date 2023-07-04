29f92775f6d8f29145062b7284f88dc27f3c291d
sheready <csstlw@gmail.com>
2023-07-04 22:38:57 +0300

    add all awards data and add require to render image paths

--- frontend/src/awardsapi.js
+++ frontend/src/awardsapi.js
@@ -8,19 +8,19 @@ const awards = [
                 name:"Edibowl Foods",
                 id:"edibowl-foods",
                 description:"While hungry for a tasty pasta meal, Edith Orego set out on a trip to the supermarket to get some homegrown, homemade pasta sauce goodness only to find an array of imported brands.This set Edith on a passionate gap-filling journey to create recipes that would incorporate some of the rich, nutritious, and flavorful natural ingredients that are grown in our beautiful country. Thus was Edibowl’s range of Sauces born.The sauces were initially made specifically to pair up with pasta dishes but a new range of Marinades and Chilli sauces was added. All Edibowl Sauces are versatile and can be used in different recipes that are both local and international.",
-                image:"./media/awards/ediblefoods.jpg",
+                img:require("./media/awards/ediblefoods.jpg"),
             },
             {
                 name: "Africa Elite Group",
                 id:"africa-elite-group",
                 description:"Established in 2010, African Elite Group is an award-winning boutique PR agency in Kenya, an innovative communication consultancy firm, and a digital marketing partner helping brands stand out, remain relevant, and win the battle. We are the people whose passion lies in giving added value to our client’s needs. As specialists in PR, we offer cutting-edge strategies as well as help you engage with your company’s targeted market. Effectively and efficiently, the African Elite Group will work by your side to perfect your ideas.",
-                image:"./media/awards/Lucia.jpeg",
+                img:require("./media/awards/Lucia.jpeg"),
             },
             {
                 name:"Ryaivas Foods",
                 id:"ryaivas-foods",
                 description:"We offer healthy breakfast cereal and granola bars. We ensure no preservatives, artificial flavors, or refined sugar that is used in our products. We have a range of muesli and granola bars.",
-                image:"./media/awards/ryaivas.jpg",
+                img:require("./media/awards/ryaivas.jpg"),
             },
         ]
         
@@ -34,24 +34,204 @@ const awards = [
                 name:"Tuendelee Foundation",
                 id:"tuendelee-foundation",
                 description:"We actualize community impact by availing scholarships, mentorship, internship, and job opportunities to the students, and then support them to impact the community via their own giving back projects. In addition to this transformation, we have recently, started a business consulting arm for SMEs which thrives on the manpower of our students to support SMEs to enhance their investor-readiness by having books of account. We therefore not only prepare the students for work but also provides work that further builds community impact, while supporting other SMEs in their financial management and growth endeavors.",
-                image:"./media/awards/Gaha.jpg",
+                img:require("./media/awards/Gaha.jpg"),
             },
             {
                 name: "Farmer On Fire Ltd",
                 id:"farmer-on-fire",
                 description:"An organization based in Nairobi, Kenya, that provides end-to-end support for smallholder farmers in Africa. It also links established agribusinesses like AGRA, John Deere, Heifer International, and the Ministry of Agriculture to their farmer community of over 50,000 Smallholder farmers that follow the brand through digital channels.",
-                image:"./media/awards/farmer.jpg",
+                img:require("./media/awards/farmer.jpg"),
             },
             {
                 name:"Beatrice Fullal Foundation",
                 id:"beatrice-fullal-foundation",
                 description:"The aim of the foundation is to ensure that through education and mentorship, we can provide the marginalized communities within our home counties a chance at decent work, sustainable towns/communities, and sustainable economic growth.",
-                image:"./media/awards/fullal.jpg",
+                img:require("./media/awards/fullal.jpg"),
             },
        
            
         ]
         
+    },
+    {
+        name: "Environmental Impact",
+        id: "environmental-impact",
+        description:"This category recognizes outstanding MSMEs with deep commitments to social justice and environmental sustainability. Our goal is to showcase such achievements to spread best practices, instigate dialogue, and inspire others – so that more such efforts are expended by larger businesses.",
+        nominees:[
+            {
+                name:"Pure Purple",
+                id:"pure-purple",
+                description:"Pure Purple is a contemporary footwear and accessories brand that is passionate about premium value addition on locally sourced leather and we do that with significant imprints in sustainability and community.",
+                img:require("./media/awards/purple.jpg"),
+            },
+            {
+                name: "Omina Otsieno Enterprises",
+                id:"omina-otsieno-enterprises",
+                description:"My name is Sylvia Omina Otsieno, founder and Creative Director of Omina Otsieno which is a fashion brand that specializes in Jewellery made from banana fibre.",
+                img:require("./media/awards/omina.jpg"),
+            },
+            {
+                name:"Harvesting For Good East Africa",
+                id:"harvesting-for-good",
+                description:"Harvesting for Good East Africa provides the necessary tools and services in helping improve access to fresh, healthy local food, using innovative approaches in resilient and regenerative agriculture known as permaculture.",
+                img:require("./media/awards/harvesting.jpg"),
+            },          
+        ]        
+    },
+    {
+        name: "Kayana Choice Awards",
+        id: "kayana-choice",
+        description:"This award recognizes business excellence based on brand reputation, professionalism, customer satisfaction, and the quality of service or product in the market. In this unique category, the winner is chosen from, and by, the Kayana community after the nominations.",
+        nominees:[
+            {
+                name: "Revolutionary Coffee",
+                id:"revolutionary-coffee",
+                description:"Revolutionary Coffee is a woman-owned business that buys, roasts, and sells Kenya’s amazing coffee. We focus on specialty coffee so that the people of this nation who put so much into our amazing beans, can enjoy export-quality coffee here at home.",
+                img:require("./media/awards/revolutionary.jpg"),
+            },
+            {
+                name:"Busu Naturals",
+                id:"busu-naturals",
+                description:"Busu Natural Skincare is a Kenyan female-owned and Female-run business; that proudly crafts premium quality, 100% natural skin moisturizers for clients with eczema issues, sensitive skin, and health-conscious individuals who are keen on what goes onto their skin.",
+                img:require("./media/awards/busu.jpg"),
+            }, 
+            {
+                name:"Brand Discovery Ltd - Stir Me Tea",
+                id:"stir-me-tea",
+                description:"Stir Me Tea is a premium Tea brand founded by Sheena Patel, making premium teas available to local consumers and the global market. Our mission is to make ours as a Kenyan tea brand a globally recognized name as Kenyan tea often loses its identity once it is bulk sold which is what the majority export of tea does.",
+                img:require("./media/awards/sheena.jpg"),
+            },
+         
+        ]        
+    },
+    {
+        name: "Most Adaptive Use of Technology",
+        id: "most-adaptive",
+        description:"Businesses have been tested in a variety of ways in the past decade and challenged to embrace technology solutions. This category honors the continuous effort by MSMEs to digitize their businesses to offer fast and efficient customer service.",
+        nominees:[
+            {
+                name:"Vivanet Solutions Ltd",
+                id:"vivanet-solutions",
+                description:"Vivanet Solutions Ltd was established in 2014 to meet the changing business requirements for managed Audio-Visual solutions. It was clear that the convergence of Digital Audio-Visual and network security created an opportunity in the market for an independent reseller with expertise in both fields.",
+                img:require("./media/awards/vivanet.jpg"),
+            }, 
+            {
+                name:"Blooming Flowers,Plants and Designs Ltd",
+                id:"blooming-flowers",
+                description:"Blooming Flowers, Plants, and Designs Limited is a prominent floral company that specializes in offering export-quality bouquets, plants, and creative designs at affordable prices. The company was established with the primary objective of providing the local community with access to high-quality floral products that were typically reserved for export markets. Additionally, it aimed to create a supportive network for florists to foster their growth and development.",
+                img:require("./media/awards/blooming.jpg"),
+            },
+            {
+                name: "Amore Business Systems",
+                id:"amore-business",
+                description:"AMORE Business Systems and Ventures Ltd (well known as Amore) is a vibrant fashion store located in Nairobi, the heart of the largest economy in East Africa. The head office is located in Amore a three-time award-winning fashion brand that leverages technology to sell clothing to the vibrant middle class and professional community with a need for quality, ready-to-wear apparel at affordable rates.  We have developed a special selling proposition that is consistently modified to meet high fashion trends as well as customers’ purchasing preferences. We aim to grow by serving our clients better, in line with our mission, as we strive to attain our vision. This venture has been in operation since 2014 and has grown its customer size by investing moderately in online marketing.",
+                img:require("./media/awards/amore.jpg"),
+            },
+
+         
+        ]        
+    },
+    {
+        name: "Most Innovative Business",
+        id: "most-innovative",
+        description:"Many businesses constantly rethink their business processes to improve on existing products and services. This category recognizes businesses that have introduced new ideas, workflows, methodologies, services, and products to meet the evolving demands of the market",
+        nominees:[
+            {
+                name:"Angie's Crochets",
+                id:"angie-crochets",
+                description:"At Angie’s Crochet, we have sought innovation in the footwear industry where we creatively design and sustainably produce sandals for ladies, gents, and kids.",
+                img:require("./media/awards/angie.jpg"),
+            }, 
+            {
+                name:"Watsy Lactose-Free Ice Cream",
+                id:"watsy-lactose",
+                description:"The ice cream is manufactured from locally sourced, quality raw materials that make for a pleasant delicacy for you and your family.Watsy Lactose Free Ice cream has grown exponentially from the one flavor at launch to have a dozen plus flavors.",
+                img:require("./media/awards/watsy.png"),
+            },
+            {
+                name: "Imani Health",
+                id:"imani-health",
+                description:"Imani Health is a savings e-wallet geared towards healthcare. We enable you to set money aside to meet either one-off healthcare needs such as delivery and recurrent healthcare needs such as healthcare for the elderly and those with chronic healthcare conditions. ",
+                img:require("./media/awards/imani.jpg"),
+            },
+
+         
+        ]        
+    },
+    {
+        name: "Most Resilient Business",
+        id: "most-resilient",
+        description:"Many businesses constantly rethink their business processes to improve on existing products and services. This category recognizes businesses that have introduced new ideas, workflows, methodologies, services, and products to meet the evolving demands of the market",
+        nominees:[
+            {
+                name:"Zanta Adeyde",
+                id:"zanta-adeyde",
+                description:"Our passion has grown by designing and creating pure leather handbags and bespoke accessories and showcasing them at different global fashion shows i.e. Unesco Africa Week in Paris, Milan Fashion Week – in Italy, African Fashion Week -in London, and most recently African fashion Seattle-USA.All our products are proudly hand-made in Kenya by a collective of young and expert craftsmen and women based in the Kibera slum. The brand directly supports more than twenty families with meaningful livelihood opportunities and support for cancer survivors.",
+                img:require("./media/awards/zanta.jpg"),
+            },
+            {
+                name:"Alata Exclusive",
+                id:"alata-exclusive",
+                description:"We profile ourselves as a leading ladies’ fashion and apparel store retailing formal office wear, casual and occasional ladies’ pants, tops, dresses, suits, and gowns. We are also the exclusive retailers for renowned shoe brands Luz Da Lua and Wirth (from Brazil) as well as Dave and Johnny Gowns (a leading fashion house in New York) In respect to scents, we mainly retail Bath and Body works products – entire beauty care, aromatherapy, and fragrance range (car scents, candles, wallflowers, etc) with a range of over 100 scents. We also stock Victoria’s Secrets beauty products, Body Shop as well as selected local beauty care brands.",
+                img:require("./media/awards/alata.jpg"),
+            }, 
+            {
+                name: "Mang Wear",
+                id:"mang-wear",
+                description:"Imani Health is a savings e-wallet geared towards healthcare. We enable you to set money aside to meet either one-off healthcare needs such as delivery and recurrent healthcare needs such as healthcare for the elderly and those with chronic healthcare conditions. ",
+                img:require("./media/awards/mang.jpg"),
+            },
+        ]        
+    },
+    {
+        name: "Strong Leadership",
+        id: "strong-leadership",
+        description:"It takes a great deal of effort to make a small business successful. We are looking for business owners who inspire their team, make responsible and calculated decisions, value the input of others, manage business finances well, embrace diversity, ensure clarity of business goals, and create a culture of teamwork.",
+        nominees:[
+            {
+                name:"Stawi Wellness Center",
+                id:"stawi-wellness",
+                description:"Our objective is to normalize mental health challenges and encourage clients to strive for intentional mental health wellbeing. To help individuals, couples, families, and groups walk through seemingly insurmountable moments and transitions and come out whole on the other side equipped to face the future.",
+                img:require("./media/awards/stawi.jpeg"),
+            },
+            {
+                name:"Peperusha Binti",
+                id:"peperusha-binti",
+                description:"Peperusha Binti is a women-led and youth-serving organization founded by Ms Sharon Jesscah, established and legally registered as a Community-Based Organization (CBO) that provides access to adolescents’ sexual reproductive health and rights information, services, and products with the aim of ending gender-based violence among young women and girls. Peperusha Binti also educates young girls on menstrual hygiene, donates sanitary towels, and creates awareness on sexual reproductive health and rights and menstrual health for the minority with health challenges in informal settlement areas of Kisumu County.",
+                img:require("./media/awards/peperusha.jpg"),
+            }, 
+            {
+                name: "Tiankara Creations",
+                id:"tiakara-creations",
+                description:"We thrive on buying Kenya build Kenya. We tailormade our manufacturing systems to fit the production of our local garments. This has been made a success by our loyal and consistent clientele. We have successfully gained their confidence and appreciation for affordability, quality, and style.",
+                img:require("./media/awards/tiankara.jpeg"),
+            },
+        ]        
+    },
+    {
+        name: "Diaspora Excellence Award",
+        id: "diaspora-excellence",
+        description:"This is a special category that recognizes exceptional women entrepreneurs who are making a positive impact in Kenya, despite being based abroad. This award honors women who have successfully launched and scaled businesses in Kenya, contributing to economic growth and social development. We are looking for entrepreneurs who demonstrate a strong commitment to their community back home, as well as a passion for entrepreneurship and innovation.",
+        nominees:[
+            {
+                name:"Ayanna Handmade",
+                id:"ayanna-handmade",
+                description:"Ayanna Handmade is a natural hair and skincare brand that has achieved great success in the industry. Founded in 2019, the brand has become known for its unique blend of natural ingredients and innovative formulations, which have proven to be highly effective for customers.",
+                img:require("./media/awards/ayana.jpg"),
+            },
+            {
+                name:"Maisha Flow",
+                id:"maisha-flow",
+                description:"Her organization Maisha Flow operating under In2EdAfrica is on a mission to provide at least a million girls in Kenya with access to uninterrupted education through the provision of sanitary products. ",
+                img:require("./media/awards/maisha-flow.jpeg"),
+            }, 
+            {
+                name: "Tlhokomelo Designs",
+                id:"tlhokomelo-designs",
+                description:"We create handcrafted, elegant, functional leather accessories to complement our customer lifestyle inspired by our African heritage. We pride ourselves in crafting items that communicate our diverse African culture, experiences, and perspective of people around the world while impacting the lives of the disadvantaged. Our products are made of leather the by-products of the meat industry. We currently produce a range of products under the “Tlhokomelo” label. This includes bags, cases, belts, and wallets",
+                img:require("./media/awards/t-designs.jpeg"),
+            },
+        ]        
     },
 
 ]
@@ -61,7 +241,7 @@ export function getSession({ sessionId, awardId }) {
       .find(({ id }) => id === awardId)
       .sessions.find(({ id }) => id === sessionId);
   }
-  
+//finds the award using the id
 export function getAward(awardId) {
     return awards.find(({ id }) => id === awardId);
 }
