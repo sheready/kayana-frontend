75ce5de8198a1b96d55468bbb6164022d6fe96d6
sheready <csstlw@gmail.com>
2023-07-05 14:31:44 +0300

    edit height image

--- frontend/src/components/Awards.js
+++ frontend/src/components/Awards.js
@@ -11,7 +11,7 @@ const Awards = () => {
             <img src={header} alt="image-header" class="h-auto lg:h-[95vh] lg:w-[100vw]" />
             <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-4 lg:mt-10">
                 <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">KAYANA FEMALE MSME AWARDS</h2>
-                <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">AWARDS CATEGORIES</p>
+                <p class="font-light text-gray-500 lg:mb-8 sm:text-xl dark:text-gray-400">AWARDS CATEGORIES</p>
             </div> 
             <div class="container flex items-center mx-auto justify-center py-4 md:py-2 flex-wrap">
                 {awards.map(award => (
