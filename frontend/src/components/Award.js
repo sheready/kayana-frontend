3b9cdad0d327cc4990fd87e516423a5c22360212
sheready <csstlw@gmail.com>
2023-07-05 14:29:14 +0300

    add award description

--- frontend/src/components/Award.js
+++ frontend/src/components/Award.js
@@ -18,13 +18,14 @@ const Award = () => {
                 <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
                     <div class="mx-auto max-w-screen-sm text-center mb-3 lg:mb-2">
                         <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">MEET OUR PARTICIPANTS</h2>
-                        <p class="font-light text-gray-500 uppercase lg:mb-16 sm:text-xl dark:text-gray-400">{award.name}</p>
+                        <p class="font-light text-gray-500 uppercase lg:mb-8 sm:text-xl dark:text-gray-400">{award.name}</p>
+                        <p class="font-light lg:mb-8 sm:text-xl dark:text-gray-400">{award.description}</p>
                     </div> 
                     <div class="grid gap-8 mb-6 lg:mb-16 lg:grid-cols-3">
                         {award?.nominees.map(nominee => (
                             <div key={nominee.id} class="flex flex-col items-center md:justify-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
                                 <div class="flex object-contain">
-                                    <img class="rounded-lg sm:rounded-none sm:rounded-l-lg lg:h-[60vh] lg:w-[100vw]" src={nominee.img} alt={nominee.name}/>
+                                    <img class="rounded-lg sm:rounded sm:rounded-l-lg lg:h-[70vh] lg:w-[100vw]" src={nominee.img} alt={nominee.name}/>
                                 </div>
                                 <div class="pt-5 flex-col h-full lg:w-fit ">
                                     <h3 class="text-xl font-bold tracking-tight dark:text-white">
